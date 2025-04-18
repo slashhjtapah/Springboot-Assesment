924f56bb6f725f2443c3f2725633de8e3f689a11
aimi.n.helwany.had <aimi.n.helwany.had@accenture.com>
2025-04-18 15:00:00 +0800

    customer

--- onlinefooddelivery/src/main/java/repository/CustomerRepository.java
+++ onlinefooddelivery/src/main/java/repository/CustomerRepository.java
@@ -1,4 +1,10 @@
 package repository;
 
-public class CustomerRepository {
+import entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer,Long> {
+
 }
---
+++ onlinefooddelivery/src/main/java/service/impl/CustomerServiceImpl.java
@@ -0,0 +1,9 @@
+package service.impl;
+
+import org.springframework.stereotype.Service;
+import service.CustomerService;
+
+@Service
+public class CustomerServiceImpl implements CustomerService {
+
+}
